3d0404b8a6d3e2be810f53e8b4e05292167470d5
David Sanders <davesque@gmail.com>
2019-06-29 16:26:19 -0600

    Remove unnecessary code

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,18 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.searchUnderCursor', () => {
-		// Get the current editor
 		let editor = vscode.window.activeTextEditor;
 		if (!editor) {
-			console.log('No active editor!');
 			return;
 		}
 
-		// Get word under cursor position
 		let wordRange = editor.document.getWordRangeAtPosition(editor.selection.start);
 		if (!wordRange) {
-			console.log('No word under the cursor!');
 			return;
 		}
 
-		// Get word text
 		let wordText = editor.document.getText(wordRange);
 
-		// Initiate search
 		vscode.commands.executeCommand('workbench.action.findInFiles', {
 			query: wordText,
 			triggerSearch: true,
