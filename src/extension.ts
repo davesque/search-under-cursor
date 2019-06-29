5f346b610d1bfd18297e938c0dd4629e94598fce
David Sanders <davesque@gmail.com>
2019-06-29 16:16:15 -0600

    Add working version of command

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('extension.searchUnderCursor', () => {
+		// Get the current editor
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			console.log('No active editor!');
+			return;
+		}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "search-under-cursor" is now active!');
+		// Get word under cursor position
+		let wordRange = editor.document.getWordRangeAtPosition(editor.selection.start);
+		if (!wordRange) {
+			console.log('No word under the cursor!');
+			return;
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		// Get word text
+		let wordText = editor.document.getText(wordRange);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		// Initiate search
+		vscode.commands.executeCommand('workbench.action.findInFiles', {
+			query: wordText,
+			triggerSearch: true,
+			matchWholeWord: true,
+			isCaseSensitive: true,
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
